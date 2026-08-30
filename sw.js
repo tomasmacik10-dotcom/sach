c8d62be6e9096005dc6d043de415b7c3ee74705e
tomasmacik10-dotcom <tomas.macik10@gmail.com>
2026-08-30 22:33:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Drzi stranku v telefone, aby fungovala aj bez signalu.
-const CACHE = 'sachove-ulohy-1787721929';
+const CACHE = 'sachove-ulohy-1788121983';
 const SUBORY = ['./', './index.html', './manifest.webmanifest',
                 './ikona-192.png', './ikona-512.png',
                 './stockfish.js', './stockfish.wasm'];
